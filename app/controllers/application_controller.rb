71b71d5e2a5e757f44cc8e40d427a7a8b7142602
mitsu733 <tubbbbbu@yahoo.co.jp>
2020-08-11 04:10:26 +0000

    top&login機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, only: [:new, :edit]
+
+  def after_sign_in_path_for(resource)
+    articles_new_path  # ログイン後に遷移するpathを設定
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path # ログアウト後に遷移するpathを設定
+  end
+
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,30 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
     
   end
+
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    article = Article.new(article_params)
+    if article.save
+      flash[:success] = "article successfully created"
+      redirect_to :root
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+  private
+  
+  
+  
+  # ストロングパラメータ
+  def article_params
+    params.require(:article).permit(:title, :body, :image)
+  end
+
 end
