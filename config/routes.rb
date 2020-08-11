92eccfc5ccae12f06c81055c9b3dee170cfee7ea
mitsu733 <tubbbbbu@yahoo.co.jp>
2020-08-11 14:51:48 +0000

    add show

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   root :to =>'articles#index'
 
   get 'articles/new' => 'articles#new'
+  get 'articles/:id' => 'articles#show', as: "article"
+  get 'articles/:id/edit' => 'articles#edit', as: "edit_article"
   post 'articles' => 'articles#create'
-
+  patch 'articles/:id' => 'articles#update', as: "update_article"
+  delete 'articles/:id' => 'articles#destroy', as: "destroy_article"
 end
 
 
