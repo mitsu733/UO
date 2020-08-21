bf275fc5a5db43340abccf1f836226fc58a15b86
mitsu733 <tubbbbbu@yahoo.co.jp>
2020-08-21 05:36:20 +0000

    fix img

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root :to =>'articles#index'
+  root 'articles#index'
 
   resources :articles, except: [:show]
   get 'articles/app_show' => 'articles#app_show', as: "app_article"
   get 'articles/illust_show' => 'articles#illust_show', as: "illust_article"
 
-  # get 'articles/new' => 'articles#new'
-  # get 'articles/:id/edit' => 'articles#edit', as: "edit_article"
-  # post 'articles' => 'articles#create'
-  # patch 'articles/:id' => 'articles#update'
-  # delete 'articles/:id' => 'articles#destroy', as: "destroy_article"
 end
 
 
